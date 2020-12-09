b4db14a2b67f9e00e68d47ccba975992d140e707
Gabriel Klavans <gabeklav@bu.edu>
2020-12-09 18:42:14 -0500

    Put user class back

--- src/User.java
+++ src/User.java
@@ -2,10 +2,5 @@ public abstract class User {
     private String name;
     private UserData userData;
 
-    public User(String name, UserData data) {
-        this.name = name;
-        this.userData = data;
-    }
-
-    
+    public User(){};
 }
